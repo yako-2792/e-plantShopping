7095e1dedda959e6d3ded4915abdee9c368e4658
Alejandro Gil <141504012+yako-2792@users.noreply.github.com>
2026-05-17 02:27:47 +0000

    Task 3: CartItems component

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,45 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    // intialize total var to 0 value
+    let total = 0;
+    cart.forEach((item) => {
+      const { quantity, cost } = item;
+      const realCost = parseFloat(item.cost.substring(1)); // converting to number type
+      total += quantity * realCost;
+    });
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e)
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }))
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }))
+    } else {
+      dispatch(removeItem(item.name))
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name))
+    // would be great to add a validation here
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const itemCost = parseFloat(item.cost.substring(1));
+    return (item.quantity * itemCost).toFixed(2);
   };
 
   return (
